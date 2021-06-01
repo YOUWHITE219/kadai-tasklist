b9189607c468bbafd2a3db707bc55467f4c1f60b
yuka.imada <nxnxyou.ox@gmail.com>
2021-06-01 07:23:54 +0000

    kadai-tasklist

---
+++ db/migrate/20210601055114_rename_title_column_to_status.rb
@@ -0,0 +1,5 @@
+class RenameTitleColumnToStatus < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :tasks, :title, :status
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_102035) do
+ActiveRecord::Schema.define(version: 2021_06_01_055114) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
+    t.string "status"
   end
 
 end
